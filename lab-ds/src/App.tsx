7ecf79a2a1bfb4195d651101ac2732ff322d48da
LostVoyager <Viktorfinger@hotmail.com>
2022-10-11 21:49:55 -0300

    Install and setup tailwindcss, postcss, autoprefixer

--- lab-ds/src/App.tsx
+++ lab-ds/src/App.tsx
@@ -1,5 +1,10 @@
+import './styles/global.css';
+
 export function App() {
   return (
-      <p>Hello world!</p>
+    <>
+      <p className='font-bold text-2xl text-violet-500'>Hello world!</p>
+      <button className='bg-violet-500 font-medium px-4 py-2 rounded text-white hover:bg-violet-600'>Enviar</button>
+    </>
   )
 }
