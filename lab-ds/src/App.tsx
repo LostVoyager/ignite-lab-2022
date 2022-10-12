def4455a4a79a6d3db84bd09ed6d74f3aaebe85d
LostVoyager <Viktorfinger@hotmail.com>
2022-10-12 20:31:21 -0300

    Heading Component

--- lab-ds/src/App.tsx
+++ lab-ds/src/App.tsx
@@ -3,7 +3,7 @@ import './styles/global.css';
 export function App() {
   return (
     <>
-      <p className='font-bold text-2xl text-violet-500'>Hello world!</p>
+      <p className='font-bold text-xl text-violet-500'>Hello world!</p>
       <button className='bg-cyan-500 font-medium px-4 py-2 rounded text-white hover:bg-cyan-300'>Enviar</button>
     </>
   )
