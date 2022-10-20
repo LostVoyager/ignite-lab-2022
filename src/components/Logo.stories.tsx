cef299e72cdaf2cb411061090d885fd7f9307521
LostVoyager <Viktorfinger@hotmail.com>
2022-10-19 23:00:23 -0300

    Add Logo Component

---
+++ src/components/Logo.stories.tsx
@@ -0,0 +1,19 @@
+import {Meta, StoryObj} from '@storybook/react'
+
+import {Logo, LogoProps} from './Logo'
+import { Text } from './Text';
+
+export default {
+  title: 'React Components/Logo',
+  component: Logo,
+  args: {
+
+  },
+  argTypes: {
+    
+  },
+} as Meta<LogoProps>
+
+export const Default: StoryObj<LogoProps> = {
+
+};
